2542119c51c7d44f233c2fb0a0754ef29feb2982
Nicholas Andrews <nicholasandrews8@gmail.com>
2023-11-23 21:36:02 -0300

    inicio do projeto

---
+++ produtos/script.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Produto inicial
+    const initialProduct = {
+        nome: "CUPCAKE",
+        precoCusto: 3,
+        precoVenda: 8
+    };
+
+    // Inicializar lista com o produto inicial
+    addProductToList(initialProduct);
+
+    // Adicionar evento ao formulário
+    const addProductForm = document.getElementById("add-product-form");
+    addProductForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        addProduct();
+    });
+});
+
+function addProduct() {
+    // Obter valores do formulário
+    const productName = document.getElementById("productName").value;
+    const productCostPrice = parseFloat(document.getElementById("productCostPrice").value);
+    const productSellingPrice = parseFloat(document.getElementById("productSellingPrice").value);
+
+    // Validar entradas
+    if (!productName || isNaN(productCostPrice) || isNaN(productSellingPrice)) {
+        alert("Por favor, preencha todos os campos corretamente.");
+        return;
+    }
+
+    // Criar objeto de produto
+    const newProduct = {
+        nome: productName,
+        precoCusto: productCostPrice,
+        precoVenda: productSellingPrice
+    };
+
+    // Adicionar produto à lista
+    addProductToList(newProduct);
+
+    // Limpar formulário
+    clearForm();
+}
+
+function addProductToList(product) {
+    // Criar elemento li para o produto
+    const productList = document.getElementById("product-list");
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `<strong>${product.nome}</strong> - Custo R$${product.precoCusto.toFixed(2)} - Venda R$${product.precoVenda.toFixed(2)}`;
+
+    // Adicionar à lista
+    productList.appendChild(listItem);
+}
+
+function clearForm() {
+    // Limpar campos do formulário
+    document.getElementById("productName").value = "";
+    document.getElementById("productCostPrice").value = "";
+    document.getElementById("productSellingPrice").value = "";
+}
